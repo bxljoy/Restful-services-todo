41bfa1da61c4d8d11004abea5a582345638b5156
bxljoy <259500313@qq.com>
2023-06-30 16:28:26 +0700

    Fixing the bug of Spring Security to Authenticate all the requests

--- src/main/java/com/alex/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/alex/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -2,11 +2,11 @@ package com.alex.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class BasicAuthenticationSecurityConfiguration {
@@ -14,7 +14,7 @@ public class BasicAuthenticationSecurityConfiguration {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
-				.authorizeHttpRequests(requests -> requests.requestMatchers(new AntPathRequestMatcher("/**"))
+				.authorizeHttpRequests(requests -> requests.antMatchers(HttpMethod.OPTIONS, "/**")
 						.permitAll()
 						.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
