4506b4a2af56dec2baa17622e941013559fad37b
bxljoy <259500313@qq.com>
2023-07-02 12:35:33 +0700

    Fixing the problem of  Access-Control-Allow-Origin

--- src/main/java/com/alex/rest/webservices/restfulwebservices/jwt/JwtAuthenticationController.java
+++ src/main/java/com/alex/rest/webservices/restfulwebservices/jwt/JwtAuthenticationController.java
@@ -3,18 +3,20 @@ package com.alex.rest.webservices.restfulwebservices.jwt;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 public class JwtAuthenticationController {
-    
+
     private final JwtTokenService tokenService;
-    
+
     private final AuthenticationManager authenticationManager;
 
-    public JwtAuthenticationController(JwtTokenService tokenService, 
+    public JwtAuthenticationController(JwtTokenService tokenService,
             AuthenticationManager authenticationManager) {
         this.tokenService = tokenService;
         this.authenticationManager = authenticationManager;
@@ -23,17 +25,17 @@ public class JwtAuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<JwtTokenResponse> generateToken(
             @RequestBody JwtTokenRequest jwtTokenRequest) {
-        
-        var authenticationToken = 
+
+        var authenticationToken =
                 new UsernamePasswordAuthenticationToken(
-                        jwtTokenRequest.username(), 
+                        jwtTokenRequest.username(),
                         jwtTokenRequest.password());
-        
-        var authentication = 
+
+        var authentication =
                 authenticationManager.authenticate(authenticationToken);
-        
+
         var token = tokenService.generateToken(authentication);
-        
+
         return ResponseEntity.ok(new JwtTokenResponse(token));
     }
 }
