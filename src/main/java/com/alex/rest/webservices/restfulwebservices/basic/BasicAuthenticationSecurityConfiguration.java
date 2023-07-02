d93228a442afe8f2e08513c7aae9bd557bd2665d
bxljoy <259500313@qq.com>
2023-07-02 12:03:38 +0700

    Using JWT and Spring Security to secure REST API

--- src/main/java/com/alex/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/alex/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -1,14 +1,13 @@
 package com.alex.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class BasicAuthenticationSecurityConfiguration {
 
 	@Bean
@@ -16,6 +15,7 @@ public class BasicAuthenticationSecurityConfiguration {
 		return http
 				.authorizeHttpRequests(requests -> requests.antMatchers(HttpMethod.OPTIONS, "/**")
 						.permitAll()
+						.requestMatchers().permitAll()
 						.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).csrf()
